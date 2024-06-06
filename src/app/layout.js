ce456fb1e37dc59fa98f7e920fb716e9271508ec
TheUnicat <matthew4680@icloud.com>
2024-06-06 10:24:54 -0700

    center stuff

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,19 +2,20 @@ import Link from 'next/link';
 import Navbar from '../components/Navbar';
 import { Analytics } from "@vercel/analytics/react"
 
+
 export default function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen bg-primary-light font-sans text-gray-900">
       <header className="bg-primary p-4 text-white flex justify-between items-center">
         <div className="flex flex-col items-center">
           <Link href="/" className="hover:scale-105 transition duration-300">
-            <h1 className="text-3xl md:text-4xl font-bold whitespace-nowrap">The Dignity Project</h1>
+            <h1 className="text-2xl md:text-4xl font-bold whitespace-nowrap">The Dignity Project</h1>
           </Link>
           <p className="mt-2">at JH</p>
         </div>
         <Navbar />
       </header>
-      <main className="flex-grow p-4">
+      <main className="flex-grow p-4 mx-auto w-full max-w-3xl">
         {children}
       </main>
       <footer className="bg-primary p-4 text-white">
@@ -23,3 +24,4 @@ export default function Layout({ children }) {
     </div>
   );
 }
+
